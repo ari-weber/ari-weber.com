9e3752181fa6c3fbb22723ecbdaffdeefb3219cb
Ari Weber <arimweber@gmail.com>
2026-06-18 15:14:26 -0400

    Use Astro implementation of Image?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import { defineConfig } from 'astro/config';
+
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   site: 'https://ari-weber.com',
+
   integrations: [
     sitemap(),
   ],
+  output: 'server',
+  adapter: vercel({
+    imageService: true, // Configures the Vercel Image Optimization API
+  }),
 });
