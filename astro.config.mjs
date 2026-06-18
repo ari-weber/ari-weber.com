b90f59e5c17b6416edd84ff639dd4696a9a5979f
Ari Weber <arimweber@gmail.com>
2026-06-18 15:39:28 -0400

    Change dependencies?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-import { defineConfig } from 'astro/config';
-
-import vercel from '@astrojs/vercel';
-
 export default defineConfig({
   site: 'https://ari-weber.com',
-
   integrations: [
     sitemap(),
   ],
-  output: 'server',
-  adapter: vercel({
-    imageService: true, // Configures the Vercel Image Optimization API
-  }),
 });
